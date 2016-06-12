0fbe163a0b5af5893613c4ea964135a9e89d88a0
amensiko <anastasija.mensikova@trincoll.edu>
2016-06-12 15:54:55 +0300

    First graph

--- js/chart.js
+++ js/chart.js
@@ -1,8 +1,8 @@
 (function() {
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 260 - margin.left - margin.right,
+    height = 100 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
