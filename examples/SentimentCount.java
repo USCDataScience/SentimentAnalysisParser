3b97c25e5ffbd50ed51ebd0f657e1e7fc9ed4817
amensiko <anastasija.mensikova@trincoll.edu>
2016-06-12 15:49:58 +0300

    Working on implementing graphs

--- examples/SentimentCount.java
+++ examples/SentimentCount.java
@@ -1,16 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+import java.io.File;
+
 public class SentimentCount {
 	
-	private static int pos;
-	private static int neg;
+	private int pos;
+	private int neg;
 	
 	public SentimentCount() {
 		this.pos = 0;
 		this.neg = 0;
 	}
 	
-	
-	
-	public static void main(String args[]) {
-		SentimentCount sc = new SentimentCount();
+	public void counter(File file) throws FileNotFoundException {
+		String content = new Scanner(file).useDelimiter("\\Z").next();
+		for (int i = 0; i < content.length() - 8; i++) {
+			if (content.substring(i, i+8).equalsIgnoreCase("positive")) {
+				this.pos++;
+			}
+			else if (content.substring(i, i+8).equalsIgnoreCase("negative")) {
+				this.neg++;
+			}
+		}
+	}
+
+		
+	public static void main(String args[]) throws FileNotFoundException {
+		SentimentCount sentCount = new SentimentCount();
+		File file = new File("./out");
+		sentCount.counter(file);
+		System.out.println("Positive: " + sentCount.pos);
+		System.out.println("Negative: " + sentCount.neg);
 	}
 }
