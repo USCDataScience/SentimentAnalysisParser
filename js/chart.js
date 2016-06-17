1ab2aa8ac0e34802dd6c8cf64c7a17c41d31bfd7
amensiko <anastasija.mensikova@trincoll.edu>
2016-06-17 17:03:37 +0300

    Countries graph

--- js/chart.js
+++ js/chart.js
@@ -57,7 +57,6 @@ d3.tsv("./data/chart1.tsv", type, function(error, data) {
       .attr("width", x.rangeBand())
       .attr("y", function(d) { return y(d.frequency); })
       .attr("height", function(d) { return height - y(d.frequency); });
-	  .style("fill", function(d) { return color(d.name); });
 });
 
 function type(d) {
