631e84567397f23f0b5cd5791aa4e96e51822a5f
amensiko <anastasija.mensikova@trincoll.edu>
2016-06-17 17:11:12 +0300

    Countries graph

--- js/countries.js
+++ js/countries.js
@@ -1,6 +1,6 @@
 (function() {
 
-var margin = {top: 20, right: 20, bottom: 30, left: 70},
+var margin = {top: 20, right: 20, bottom: 30, left: 80},
     width = 1000 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
