25db78a3eef5b19a604804dc4baca8f56adb062a
amensiko <anastasija.mensikova@trincoll.edu>
2016-06-17 17:46:06 +0300

    Countries graph

--- js/countries.js
+++ js/countries.js
@@ -61,7 +61,7 @@ d3.json("./data/countries.json", function(error, dataObj) {
 
   svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", "translate(0," + 1 + ")")
       .call(xAxis);
 
   svg.append("g")
