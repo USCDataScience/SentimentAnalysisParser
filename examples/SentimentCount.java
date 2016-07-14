541b83fca42eebaeaf7971610c3b7d5fef57584b
amensiko <anastasija.mensikova@trincoll.edu>
2016-07-14 13:32:21 +0200

    Added ready-to-use binary and categorical models

--- examples/SentimentCount.java
+++ examples/SentimentCount.java
@@ -4,22 +4,37 @@ import java.io.File;
 
 public class SentimentCount {
 	
-	private int pos;
-	private int neg;
+	private int angry;
+	private int sad;
+	private int neutral;
+	private int like;
+	private int love;
 	
 	public SentimentCount() {
-		this.pos = 0;
-		this.neg = 0;
+		this.angry = 0;
+		this.sad = 0;
+		this.neutral = 0;
+		this.like = 0;
+		this.love = 0;
 	}
 	
 	public void counter(File file) throws FileNotFoundException {
 		String content = new Scanner(file).useDelimiter("\\Z").next();
 		for (int i = 0; i < content.length() - 8; i++) {
-			if (content.substring(i, i+8).equalsIgnoreCase("positive")) {
-				this.pos++;
+			if (content.substring(i, i+5).equalsIgnoreCase("angry")) {
+				this.angry++;
 			}
-			else if (content.substring(i, i+8).equalsIgnoreCase("negative")) {
-				this.neg++;
+			else if (content.substring(i, i+3).equalsIgnoreCase("sad")) {
+				this.sad++;
+			}
+			else if (content.substring(i, i+7).equalsIgnoreCase("neutral")) {
+				this.neutral++;
+			}
+			else if (content.substring(i, i+4).equalsIgnoreCase("like")) {
+				this.like++;
+			}
+			else if (content.substring(i, i+4).equalsIgnoreCase("love")) {
+				this.love++;
 			}
 		}
 	}
@@ -27,9 +42,13 @@ public class SentimentCount {
 		
 	public static void main(String args[]) throws FileNotFoundException {
 		SentimentCount sentCount = new SentimentCount();
-		File file = new File("./out1");
+		File file = new File("./categ-out");
 		sentCount.counter(file);
-		System.out.println("Positive: " + sentCount.pos);
-		System.out.println("Negative: " + sentCount.neg);
+		System.out.println("Angry: " + sentCount.angry);
+		System.out.println("Sad: " + sentCount.sad);
+		System.out.println("Neutral: " + sentCount.neutral);
+		System.out.println("Like: " + sentCount.like);
+		System.out.println("Love: " + sentCount.love);
+		
 	}
 }
