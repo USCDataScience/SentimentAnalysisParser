74efeb42ec3caaa4ee04cb749f43e8ff400b8ceb
amensiko <anastasija.mensikova@trincoll.edu>
2016-08-03 00:35:01 +0300

    Negative data graphs added

---
+++ js/ht-categ-stanford-new.js
@@ -0,0 +1,69 @@
+(function() {
+
+var margin = {top: 20, right: 20, bottom: 30, left: 65},
+    width = 550 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+	
+var color = d3.scale.ordinal()
+	.range(["#6b486b", "#ff8c00"]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+var svg = d3.select("#ht-stan-new").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("./data/ht_data_stanford_new.tsv", type, function(error, data) {
+  if (error) throw error;
+
+  x.domain(data.map(function(d) { return d.sentiment; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("# of reviews");
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.sentiment); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); });
+	  //.style("fill", function(d) { return d.color; });
+	  //.style("fill", function(d) { return color(d.name); });
+	  //.style("fill", color);
+});
+
+function type(d) {
+  d.frequency = +d.frequency;
+  return d;
+}
+}())
