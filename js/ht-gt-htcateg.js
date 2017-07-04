ff19393310c4f8f80b1d42553158494b69083682
Menshikova <anastasija.mensikova@trincoll.edu>
2017-07-03 18:52:44 -0700

    Added HT LG Ground Truth graphs and table

---
+++ js/ht-gt-htcateg.js
@@ -0,0 +1,117 @@
+(function() {
+
+var margin = {top: 20, right: 20, bottom: 30, left: 65},
+    width = 900 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+
+var x0 = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var x1 = d3.scale.ordinal();
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.ordinal()
+	.range(["#6b486b", "#ff8c00", "#ed2998", "#29a8ed", "#c40f0f"]);
+    //.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var xAxis = d3.svg.axis()
+    .scale(x0)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(d3.format(5));
+
+var svg = d3.select("#ht-gt-htcateg").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("./data/ht-gt-htcat.json", function(error, dataObj) {
+  if (error) throw error;
+  
+  var data = [];
+  for (var p in dataObj) {
+	  var truth = dataObj[p];
+	  truth.name = p;
+	  //var positive =  p.prediction.positive;
+	  //var negative = p.prediction.negative;
+	  //var sentiment = [positive, negative];
+	  truth.sentiment = [];
+	  truth.sentiment.push({ name: "angry", value: truth.prediction.angry || 0});
+	  truth.sentiment.push({ name: "sad", value: truth.prediction.sad || 0});
+	  truth.sentiment.push({ name: "neutral", value: truth.prediction.neutral || 0});
+	  truth.sentiment.push({ name: "like", value: truth.prediction.like || 0});
+	  truth.sentiment.push({ name: "love", value: truth.prediction.love || 0});
+	  //country.prediction = sentiment;
+	  data.push(truth);
+  }
+
+  //var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
+
+  //data.forEach(function(d) {
+  //  d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+  //});
+  var sentNames = ["angry", "sad", "neutral", "like", "love"];
+
+  x0.domain(data.map(function(d) { return d.name; }));
+  x1.domain(sentNames).rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(data, function(d) { return d3.max(d.sentiment, function(d) { return d.value; }); })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+	  .attr("transform", "translate(" + (-15) + ",0)")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("# of ads");
+	  
+  var truthData = svg.selectAll(".truth")
+      .data(data)
+    .enter().append("g")
+      .attr("class", "truth")
+      .attr("transform", function(d) { return "translate(" + x0(d.name) + ",0)"; });
+
+  truthData.selectAll("rect")
+      .data(function(d) { return d.sentiment; })
+    .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d) { return x1(d.name); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .style("fill", function(d) { return color(d.name); });
+
+  var legend = svg.selectAll(".legend")
+      .data(sentNames.slice().reverse())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+
+});
+
+}())
