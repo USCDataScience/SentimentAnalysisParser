70bef814e495a2520e3f6b5575a556f5544b95e4
Menshikova <anastasija.mensikova@trincoll.edu>
2017-08-14 08:51:13 -0700

    Working on b&w graphs

--- js/ht-gt-netflix.js
+++ js/ht-gt-netflix.js
@@ -6,6 +6,16 @@ var svg = d3.select("#ht-gt-netflix").append("svg"),
     height = 500,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 	
+function fill(d) {
+	var name = d.name || d;
+	if(name === 'positive') {
+		return 'url(#diagonalHatch)';
+	} else if(name === 'negative') {
+		return 'url(#crosshatch)';
+	}
+	return null;
+}	
+
 var x0 = d3.scaleBand()
 	.rangeRound([0, width])
 	.paddingInner(0.1);
@@ -41,22 +51,13 @@ d3.json("./data/ht-gt-netflix.json", function(error, dataObj) {
   for (var p in dataObj) {
 	  var truth = dataObj[p];
 	  truth.name = p;
-	  //var positive =  p.prediction.positive;
-	  //var negative = p.prediction.negative;
-	  //var sentiment = [positive, negative];
 	  truth.sentiment = [];
 	  truth.sentiment.push({ name: "positive", value: truth.prediction.positive || 0});
 	  truth.sentiment.push({ name: "negative", value: truth.prediction.negative || 0});
-	  //country.prediction = sentiment;
 	  data.push(truth);
 	  
   }
 
-  //var ageNames = d3.keys(data[0]).filter(function(key) { return key !== "State"; });
-
-  //data.forEach(function(d) {
-  //  d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
-  //});
   var sentNames = ["positive", "negative"];
 
   x0.domain(data.map(function(d) { return d.name; }));
@@ -89,9 +90,21 @@ d3.json("./data/ht-gt-netflix.json", function(error, dataObj) {
     	.attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
     	.attr('stroke', '#000000')
     	.attr('stroke-width', 1);
+		
+  g.append('defs')
+   .append('pattern')
+    	.attr('id', 'crosshatch')
+    	.attr('patternUnits', 'userSpaceOnUse')
+    	.attr('width', 8)
+    	.attr('height', 8)
+   .append('path')
+    	//.attr('d', 'M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2')
+    	.attr('d', 'M2,2 l5,5')
+    	.attr('stroke', '#000000')
+    	.attr('stroke-width', 1);
 	  
-  var fill = d3.scaleOrdinal()
-		.range(["##diagonalHatch"]);//, "#circles"]);
+//  var fill = d3.scaleOrdinal()
+//		.range(["##diagonalHatch"]);//, "#circles"]);
   //var truthData = g.append("g")//.selectAll(".truth")
   var truthData = g.append("g")
 	  .selectAll("g")
@@ -109,7 +122,7 @@ d3.json("./data/ht-gt-netflix.json", function(error, dataObj) {
       .attr("x", function(d) { return x1(d.name); })
       .attr("y", function(d) { return y(d.value); })
       .attr("height", function(d) { return height - y(d.value); })
-	  .attr('fill', 'url(#diagonalHatch)');
+	  .attr('fill', fill);
       //.style("fill", function(d) { return fill(d.name); });
 
   var legend = g.append("g")//.selectAll(".legend")
@@ -129,7 +142,7 @@ d3.json("./data/ht-gt-netflix.json", function(error, dataObj) {
       .attr("x", width - 18)
       .attr("width", 18)
       .attr("height", 18)
-	  .attr('fill', 'url(#diagonalHatch)');
+	  .attr('fill', fill);  //'url(#diagonalHatch)'
 
   legend.append("text")
       .attr("x", width - 24)
